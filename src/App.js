f8787fd2d5b8eb16fc7097948fc07e5c14160f19
Thomas Eitrheim <teitrheim@gmail.com>
2024-08-26 14:49:11 +0200

    Update import paths and add Header component
    The import paths in `App.js` have been updated to reflect the changes in the file structure. Additionally, the `Header` component has been added to the `App` component to display a header section on each page.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
-import About from "./Pages/About";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "../../images/logo.png";
+import { Link } from "react-router-dom";
+import "./Header.css";
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src={logo} alt="Holidaze Logo" style={{ height: "50px" }} />
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/accommodation">Accommodation</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
