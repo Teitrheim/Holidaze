86b6da35c90356ca14d5de9480c4ffec8cc6e424
Thomas Eitrheim <teitrheim@gmail.com>
2024-08-26 13:04:49 +0200

    Update react-router-dom to version 6 and fix import paths in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </Router>
   );
 }
