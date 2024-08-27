ce02ebe18a93235aad87d855b2b489494c11d57f
Thomas Eitrheim <teitrheim@gmail.com>
2024-08-27 12:35:42 +0200

    Header component styling and add mobile navigation functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../images/logo.png";
 import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="header">
       <div className="logo">
         <img src={logo} alt="Holidaze Logo" style={{ height: "50px" }} />
       </div>
-      <nav>
+      <div className="menu-icon" onClick={toggleMenu}>
+        <div className={`hamburger ${isOpen ? "open" : ""}`}></div>
+      </div>
+      <nav className={`nav ${isOpen ? "open" : ""}`}>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" onClick={toggleMenu}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/accommodation">Accommodation</Link>
+            <Link to="/accommodation" onClick={toggleMenu}>
+              Accommodation
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact Us</Link>
+            <Link to="/contact" onClick={toggleMenu}>
+              Contact Us
+            </Link>
           </li>
           <li>
-            <Link to="/login">Login</Link>
+            <Link to="/login" onClick={toggleMenu}>
+              Login
+            </Link>
           </li>
         </ul>
       </nav>
